8124f29a0472d8c1e9d68dc511ada9f5409ba711
Karl <karlnino.bautista@tup.edu.ph>
2025-11-18 19:52:50 +0800

    added framer motion in about us

--- Portfolio/src/components/AboutMe.jsx
+++ Portfolio/src/components/AboutMe.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { mixVisibility, motion, useAnimation } from "framer-motion"
+import { useInView } from 'react-intersection-observer'
 const AboutMe = () => {
+
+    const controls = useAnimation();
+    const { ref, inView } = useInView({
+        threshold: 0.3,
+    })
+
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        } else {
+            controls.start("hidden");
+        }
+    }, [inView, controls])
+
+
   return (
     <section className='w-full h-[50vh] bg-[#111827] flex justify-center '>
-        <div className='w-[70%] h-full py-10 px-5 flex flex-col gap-5 items-center justify-center' >
+        
+        <motion.div className='w-[70%] h-full py-10 px-5 flex flex-col gap-5 items-center justify-center' 
+         ref={ref}
+        variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 }
+        }}
+        initial="hidden"
+        animate={controls}
+        transition={{ duration: 0.6, ease: "easeOut" }}>
             <h1 className='text-4xl text-[#F9FAFB] font-extrabold'>About Me</h1>
             <div className='w-full h-50'>
                 <p className='text-2xl text-[#F9FAFB] font-bold '>I'm a 4th-year Bachelor of Science in Information Technology student 
@@ -11,7 +36,7 @@ const AboutMe = () => {
                     I specialize in building interactive web applications using React, JavaScript, Tailwind CSS, and Express. I enjoy problem-solving, learning new technologies, and developing projects that provide real value to users.
                 </p>
             </div>
-        </div>
+        </motion.div>
 
     </section>
   )
