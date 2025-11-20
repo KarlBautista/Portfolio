35b9da94acd63cd059dfac7fdce625f22128d506
Karl <karlnino.bautista@tup.edu.ph>
2025-11-20 20:55:41 +0800

    added responsiveness in certifications

--- Portfolio/src/components/Certificiations.jsx
+++ Portfolio/src/components/Certificiations.jsx
@@ -19,7 +19,7 @@ const Certificiations = ({ certificationsRef }) => {
 
   return (
     <section className='w-full h-auto  flex justify-center bg-[#111827] py-10 scroll-mt-38' ref={certificationsRef}> 
-            <motion.div className='w-[75%] h-full flex flex-col p-5 items-center gap-5'
+            <motion.div className='w-full md:w-[75%] h-full flex flex-col p-5 items-center gap-5'
             ref={inViewRef}
             variants={{
                 hidden: { opacity: 0, y: 150 },
@@ -28,8 +28,8 @@ const Certificiations = ({ certificationsRef }) => {
             animate={controls}
             initial="hidden"
              transition={{ duration: 0.3, ease: "easeOut" }}>
-              <h1 className='text-3xl text-[#F9FAFB] font-extrabold'>Certifications</h1>
-              <p className='text-2xl text-[#F9FAFB] font-extrabold text-center'>These certifications strengthen my foundation by validating my understanding of core web development concepts and practical skills in Javascript and responsive design.</p>
+              <h1 className='text-2xl md:text-3xl text-[#F9FAFB] font-extrabold'>Certifications</h1>
+              <p className='text-[20px] md:text-2xl text-[#F9FAFB] font-extrabold text-center'>These certifications strengthen my foundation by validating my understanding of core web development concepts and practical skills in Javascript and responsive design.</p>
               <div className='w-full h-full flex justify-center items-center'>
                   <CertCarousel />
               </div>
